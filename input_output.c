29203b3517745852aab0450752aab501fd05e517
arnob-das <contact.arnobdas@gmail.com>
2021-12-03 00:15:28 +0600

    input and output basic with comment

--- input_output.c
+++ input_output.c
@@ -3,10 +3,10 @@ int main()
 {
     int first_num, second_num;
 
-    scanf("%d %d", &first_num, &second_num);
+    scanf("%d %d", &first_num, &second_num); // input the value of first and second number 
 
-    printf("First Num Is %d\n", first_num);
-    printf("Second Num Is %d\n", second_num);
+    printf("First Num Is %d\n", first_num); // prints the first number
+    printf("Second Num Is %d\n", second_num); // prints the second number
 
     return 0;
 }
