52f0e7b6eac7eed8963615bc4dc4697c10f69eb0
arnob-das <contact.arnobdas@gmail.com>
2022-01-15 11:49:20 +0600

    new file added

--- 2. multiplication table.c
+++ 2. multiplication table.c
@@ -5,7 +5,7 @@ Semester : 1/1
 Group : C2
 ID : 20210104148
 */
-  
+
 /*
    Write a program to display the
    multiplication table of a given integer
@@ -18,12 +18,13 @@ ID : 20210104148
 int main()
 {
   int n, i;
-  
+
   printf("Enter the multiplication Number\n");
   scanf("%d", &n);
-  
-  for(i = 1; i<= 10; i++){
-    printf("%d * %d = %d\n", n, i, n*i);
+
+  for (i = 1; i <= 10; i++)
+  {
+    printf("%d * %d = %d\n", n, i, n * i);
   }
   return 0;
 }
---
+++ first.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main()
+{
+    printf("Vogoban Kripa Koro");
+}
