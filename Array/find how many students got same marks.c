9fcd4f74dfba818e1269ce980638c9c2251a8d82
arnob-das <contact.arnobdas@gmail.com>
2022-01-17 02:51:08 +0600

    array done

--- Array/find how many students got same marks.c
+++ Array/find how many students got same marks.c
@@ -18,19 +18,24 @@ int main()
 
     // checking how many students got same marks
 
+    // marks could be 0 to 100 and total value of this range is 101.
     for (i = 0; i <= 101; i++)
     {
+        // Here, marks is the index position of the array named "marks_count" and at every marks, we set the marks_count value to 0 (Zero).
         marks_count[i] = 0;
     }
 
     for (i = 0; i < 40; i++)
     {
+        //for every student's, we get his/her marks and for every student's marks(index position of "marks_count") we increase the value of marks_count.
+
         marks_count[total_marks[i]]++;
     }
 
+    // print marks count
     for (i = 0; i <= 100; i++)
     {
-        if (marks_count[i] > 0)
+        if (marks_count[i] > 0) // to avoid marks which no student get.
         {
             printf("Marks: %d \t Counts: %d\n", i, marks_count[i]);
         }
